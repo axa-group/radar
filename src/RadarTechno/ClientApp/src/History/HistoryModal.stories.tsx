2c52240d62df9019004a337cb8c9c4c81bceace8
Guillaume Chervet <guillaume.chervet@axa.fr>
2019-08-08 18:08:39 +0200

    WIP

--- src/RadarTechno/ClientApp/src/History/HistoryModal.stories.tsx
+++ src/RadarTechno/ClientApp/src/History/HistoryModal.stories.tsx
@@ -14,8 +14,7 @@ const history = [
     elementId: "5823926b69170e15d895fda0",
     entityId: null,
     // tslint:disable-next-line: max-line-length
-    diff:
-      "{\r\n  'Id': [\r\n    '5823926b69170e15d895fda0'\r\n  ],\r\n  'Version': [\r\n    0\r\n  ],\r\n  'Name': [\r\n    'Solution de parametrage'\r\n  ],\r\n  'Key': [\r\n    'solution-de-parametrage'\r\n  ],\r\n  'Category': [\r\n    'tools'\r\n  ],\r\n  'Description': [\r\n    'test'\r\n  ],\r\n  'Scope': [\r\n    'qqqq'\r\n  ],\r\n  'Reporter': [\r\n    null\r\n  ],\r\n  'UpdateDate': [\r\n    '2019-03-01T14:21:38.374Z'\r\n  ]\r\n}",
+    diff: "{\r\n  'Id': [\r\n    '5823926b69170e15d895fda0'\r\n  ],\r\n  'Version': [\r\n    0\r\n  ],\r\n  'Name': [\r\n    'Solution de parametrage'\r\n  ],\r\n  'Key': [\r\n    'solution-de-parametrage'\r\n  ],\r\n  'Category': [\r\n    'tools'\r\n  ],\r\n  'Description': [\r\n    'test'\r\n  ],\r\n  'Scope': [\r\n    'qqqq'\r\n  ],\r\n  'Reporter': [\r\n    null\r\n  ],\r\n  'UpdateDate': [\r\n    '2019-03-01T14:21:38.374Z'\r\n  ]\r\n}",
     updateDate: null
   }
 ];
