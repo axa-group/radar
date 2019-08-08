128f5d43768d18ecc8f5a61088fc0ef931f4cfe3
Guillaume Chervet <guillaume.chervet@axa.fr>
2019-08-08 18:00:27 +0200

    WIP

--- src/RadarTechno/ClientApp/src/Technology/ViewTechnologyModal.stories.tsx
+++ src/RadarTechno/ClientApp/src/Technology/ViewTechnologyModal.stories.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import { action } from '@storybook/addon-actions';
-import { boolean, object, withKnobs } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/react';
+import { action } from "@storybook/addon-actions";
+import { boolean, object, withKnobs } from "@storybook/addon-knobs";
+import { storiesOf } from "@storybook/react";
 
-import { ViewTechnologyModal } from '../Technology/ViewTechnologyModal';
-import { ITechnology } from './Technology';
+import { ViewTechnologyModal } from "../Technology/ViewTechnologyModal";
+import { ITechnology } from "./Technology";
 
 const technology: ITechnology = {
-  updateDate: new Date("2019-02-27T15:28:49.294Z"),
+  updateDate: null,
   id: "5823926b69170e15d895fda0",
   version: 0,
   category: "tools",
@@ -17,18 +17,18 @@ const technology: ITechnology = {
   scope: "scope",
   description: "description",
   entitiesStatus: [
-    {entityId: "1", entityName: "AXA France", status: "assess"},
-    {entityId: "2", entityName: "AXA UK", status: "reinforce"}
+    { entityId: "1", entityName: "AXA France", status: "assess" },
+    { entityId: "2", entityName: "AXA UK", status: "reinforce" }
   ],
   groupStatus: "reinforce"
 };
 
-storiesOf('Technologies', module)
+storiesOf("Technologies", module)
   .addDecorator(withKnobs)
-  .add('View technology modal', () => 
-  <ViewTechnologyModal 
-    item={object('technology', technology)}
-    isOpen={boolean('isOpen', true)}
-    onClose={action('onClose')}
-  />
-);
+  .add("View technology modal", () => (
+    <ViewTechnologyModal
+      item={object("technology", technology)}
+      isOpen={boolean("isOpen", true)}
+      onClose={action("onClose")}
+    />
+  ));
