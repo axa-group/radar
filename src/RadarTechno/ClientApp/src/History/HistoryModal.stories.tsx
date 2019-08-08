afa6cb22724cefa9cfefc0247944e78be2c440ae
Guillaume Chervet <guillaume.chervet@axa.fr>
2019-08-08 16:55:30 +0000

    chore(radar) set up ci (#17)
    #17

--- src/RadarTechno/ClientApp/src/History/HistoryModal.stories.tsx
+++ src/RadarTechno/ClientApp/src/History/HistoryModal.stories.tsx
@@ -1,34 +1,34 @@
-import React from 'react';
+import React from "react";
 
-import { action } from '@storybook/addon-actions';
-import { boolean, text, withKnobs } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/react';
+import { action } from "@storybook/addon-actions";
+import { boolean, text, withKnobs } from "@storybook/addon-knobs";
+import { storiesOf } from "@storybook/react";
 
-import { HistoryModal } from '../History';
+import { HistoryModal } from "../History";
 
 const history = [
-  { 
-    id: '5ccaec7275dbbc092cb9029b', 
-    author: 'Test@axa.fr', 
-    type: 'technology', 
-    elementId: '5823926b69170e15d895fda0', 
-    entityId: null, 
+  {
+    id: "5ccaec7275dbbc092cb9029b",
+    author: "Test@axa.fr",
+    type: "technology",
+    elementId: "5823926b69170e15d895fda0",
+    entityId: null,
     // tslint:disable-next-line: max-line-length
-    diff: '{\r\n  \'Id\': [\r\n    \'5823926b69170e15d895fda0\'\r\n  ],\r\n  \'Version\': [\r\n    0\r\n  ],\r\n  \'Name\': [\r\n    \'Solution de parametrage\'\r\n  ],\r\n  \'Key\': [\r\n    \'solution-de-parametrage\'\r\n  ],\r\n  \'Category\': [\r\n    \'tools\'\r\n  ],\r\n  \'Description\': [\r\n    \'test\'\r\n  ],\r\n  \'Scope\': [\r\n    \'qqqq\'\r\n  ],\r\n  \'Reporter\': [\r\n    null\r\n  ],\r\n  \'UpdateDate\': [\r\n    \'2019-03-01T14:21:38.374Z\'\r\n  ]\r\n}', 
-    updateDate: new Date('2019-05-02T13:11:14.181+0000')
+    diff: "{\r\n  'Id': [\r\n    '5823926b69170e15d895fda0'\r\n  ],\r\n  'Version': [\r\n    0\r\n  ],\r\n  'Name': [\r\n    'Solution de parametrage'\r\n  ],\r\n  'Key': [\r\n    'solution-de-parametrage'\r\n  ],\r\n  'Category': [\r\n    'tools'\r\n  ],\r\n  'Description': [\r\n    'test'\r\n  ],\r\n  'Scope': [\r\n    'qqqq'\r\n  ],\r\n  'Reporter': [\r\n    null\r\n  ],\r\n  'UpdateDate': [\r\n    '2019-03-01T14:21:38.374Z'\r\n  ]\r\n}",
+    updateDate: null
   }
 ];
 
-storiesOf('SideModal', module)
-.addDecorator(withKnobs)
-.add('History modal', () => 
-  <HistoryModal
-    history={history}
-    loading={boolean('loading', false)}
-    isOpen={boolean('isOpen', true)}
-    onClose={action('onClose')}
-    onToggle={action('onToggle')}
-    title={text('title', 'title')}
-    idToOpen={text('idToOpen', 'idToOpen')}
-  />
-);
+storiesOf("SideModal", module)
+  .addDecorator(withKnobs)
+  .add("History modal", () => (
+    <HistoryModal
+      history={history}
+      loading={boolean("loading", false)}
+      isOpen={boolean("isOpen", true)}
+      onClose={action("onClose")}
+      onToggle={action("onToggle")}
+      title={text("title", "title")}
+      idToOpen={text("idToOpen", "idToOpen")}
+    />
+  ));
--- src/RadarTechno/ClientApp/src/generic/dateStringHandler.ts
+++ src/RadarTechno/ClientApp/src/generic/dateStringHandler.ts
@@ -1,9 +1,11 @@
-import { DateTime } from 'luxon';
+import { DateTime } from "luxon";
 
-const dateTimeReviver = (value) => {
+const dateTimeReviver = value => {
   let a;
-  if (typeof value ==='string') {
-    a = /^\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)*/.exec(value);
+  if (typeof value === "string") {
+    a = /^\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)*/.exec(
+      value
+    );
     if (a) {
       return new Date(value);
     }
@@ -11,21 +13,24 @@ const dateTimeReviver = (value) => {
   return value;
 };
 
-export const convertStringDateToDateObject = (origin) => {
-  if (typeof origin ==='string') {
+export const convertStringDateToDateObject = origin => {
+  if (typeof origin === "string") {
     return dateTimeReviver(origin);
   }
   for (const propertyName in origin) {
-    if (typeof origin[propertyName] ==='string') {
+    if (typeof origin[propertyName] === "string") {
       origin[propertyName] = dateTimeReviver(origin[propertyName]);
-    } else if (typeof origin[propertyName] === 'object') {
+    } else if (typeof origin[propertyName] === "object") {
       convertStringDateToDateObject(origin[propertyName]);
     }
   }
   return origin;
 };
 
-export const dateObjectToString = (date) => {
+export const dateObjectToString = date => {
+  if (!date) {
+    return "-";
+  }
   const dateTime = DateTime.fromJSDate(date);
-  return dateTime.invalid ? date : dateTime.toFormat('dd LLL yyyy hh:mm:ss');
+  return dateTime.invalid ? date : dateTime.toFormat("dd LLL yyyy hh:mm:ss");
 };
