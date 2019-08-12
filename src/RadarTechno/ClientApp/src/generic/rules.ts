5ba43cb7dfcce0f399d85c288c376e1d97e10816
Guillaume Chervet <guillaume.chervet@axa.fr>
2019-08-12 10:00:56 +0200

    maj

--- src/RadarTechno/ClientApp/src/generic/rules.ts
+++ src/RadarTechno/ClientApp/src/generic/rules.ts
@@ -1,39 +1,44 @@
-import { MSG_REQUIRED } from '../constants';
+import { MSG_REQUIRED } from "../constants";
 
 export const rulesRequired = {
   required: {
-    message: MSG_REQUIRED,
-  },
+    message: MSG_REQUIRED
+  }
 };
 
-export const rulesMinLength = (minLength= 2) => {
+export const rulesMinLength = (minLength = 2) => {
   return {
     minLength: {
       minLength,
-      message: "This field should have at least {maxLength} characters",
-    },
-  }; 
+      message: "This field should have at least {maxLength} characters"
+    }
+  };
 };
 
 export const rulesMaxLength = (maxLength = 256) => {
   return {
-  maxLength: {
-    maxLength,
-    message: "This field should have les than {maxLength} characters",
-  }};
+    maxLength: {
+      maxLength,
+      message: "This field should have les than {maxLength} characters"
+    }
+  };
 };
 
 export const rulesMail = {
-    email: {
-      message: "Please enter a valid email address",
-    }};
+  email: {
+    message: "Please enter a valid email address"
+  }
+};
 
 export const rulesUrl = {
-    url: {
-        message: "Please enter a valid url",
-    }};
+  url: {
+    message: "Please enter a valid url"
+  }
+};
 
-export const rulesNoSpecialChars =  {
-    firstname: {
-      message: "This field do not accept specials characters",
-    }};
+export const rulesNoSpecialChars = {
+  pattern: {
+    regex: /^[a-zA-Zà-ïæù-üò-öŐ-œÿ -]*$/,
+    message: "This field do not accept specials characters"
+  }
+};
