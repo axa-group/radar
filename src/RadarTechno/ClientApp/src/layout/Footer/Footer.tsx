dbadae2d5b37880051d230c8b9de8ac7f04a6a17
Guillaume Chervet <guillaume.chervet@axa.fr>
2019-12-10 10:39:26 +0100

    feature(version): inject version dynamicaly (#121)
    * feature(version): inject version dynamicaly
    
    * fix

--- src/RadarTechno/ClientApp/src/layout/Footer/Footer.tsx
+++ src/RadarTechno/ClientApp/src/layout/Footer/Footer.tsx
@@ -2,8 +2,16 @@ import { Footer } from '@axa-fr/react-toolkit-all';
 import logo from '@axa-fr/react-toolkit-core/dist/assets/logo-axa.svg';
 import React from 'react';
 import './Footer.scss';
+
+// tslint:disable-next-line
+const data = require('../../version.json');
+
 const FooterApp = () => {
-  return <Footer icon={logo} />;
+  return (
+    <Footer icon={logo}>
+      @ 2019 AXA Tous droits réservés - {data.version}
+    </Footer>
+  );
 };
 
 export default FooterApp;
